b0a68866bd5730f3068a09bc45acd326c46379f6
WatermelonMojito <104700587+WatermelonMojito@users.noreply.github.com>
2024-06-08 00:53:16 -0500

    fixes rounding issues. Still has 0 coordinate no place bug

--- src/main/java/watermelonmojito/cloudboots/Cloud.java
+++ src/main/java/watermelonmojito/cloudboots/Cloud.java
@@ -2,21 +2,16 @@ package watermelonmojito.cloudboots;
 
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.util.helper.MathHelper;
-import net.minecraft.core.util.phys.Vec3d;
 import net.minecraft.core.world.World;
 
 public class Cloud {
 
-	public static Vec3d roundVec3d(Vec3d vec3d){
-		return Vec3d.createVector(Math.round(vec3d.xCoord), Math.floor(vec3d.yCoord), Math.round(vec3d.zCoord));
-	}
-
 	public static void placeCloud(World world, EntityPlayer player) {
 
 		//Get the players feet position
-		double playerPosX = player.getPosition(1).xCoord;
-		double playerPosY = player.getPosition(1).yCoord - 1;
-		double playerPosZ = player.getPosition(1).zCoord;
+		double playerPosX = player.getPosition(0.5F).xCoord;
+		double playerPosY = player.getPosition(0.5F).yCoord - 1;
+		double playerPosZ = player.getPosition(0.5F).zCoord;
 
 		//Find the difference between the player's position and the last position
 		double movementX = player.xo - player.x;
@@ -31,18 +26,39 @@ public class Cloud {
 			double nextPlayerPosZ = playerPosZ;
 
 			//keep adding the movement to the players location till the next block is found
-			while ((MathHelper.floor_double(playerPosX) == MathHelper.floor_double(nextPlayerPosX)) &&
+/*			while ((MathHelper.floor_double(playerPosX) == MathHelper.floor_double(nextPlayerPosX)) &&
 				   (MathHelper.floor_double(playerPosZ) == MathHelper.floor_double(nextPlayerPosZ))) {
 
 				nextPlayerPosX = nextPlayerPosX - movementX;
 				nextPlayerPosZ = nextPlayerPosZ - movementZ;
-			}
+			}*/
 
 
-			//Is the block below the next player pos air, if so fill it in with DIAMOND
+			//Is the block below the next player pos air. If statements are to correct for funky coordinate rounding imprecision
 			double blockBelowPosX = Math.floor(nextPlayerPosX);
 			double blockBelowPosY = Math.floor(nextPlayerPosY);
 			double blockBelowPosZ = Math.floor(nextPlayerPosZ);
+
+			//-x -z
+			if(playerPosX < 0 && playerPosZ < 0) {
+				blockBelowPosX = Math.floor(nextPlayerPosX + 1);
+				blockBelowPosZ = Math.floor(nextPlayerPosZ + 1);
+			}
+			//+x -z
+			else if (playerPosX > 0 && playerPosZ < 0) {
+				blockBelowPosX = Math.floor(nextPlayerPosX);
+				blockBelowPosZ = Math.floor(nextPlayerPosZ + 1);
+			}
+			//-x +z
+			else if (playerPosX < 0 && playerPosZ > 0) {
+				blockBelowPosX = Math.floor(nextPlayerPosX + 1);
+				blockBelowPosZ = Math.floor(nextPlayerPosZ);
+			}
+			//+x +z
+			else if (playerPosX > 0 && playerPosZ > 0) {
+				blockBelowPosX = Math.floor(nextPlayerPosX);
+				blockBelowPosZ = Math.floor(nextPlayerPosZ);
+			}
 
 			//to resolve issues with negative positions
 			if (blockBelowPosZ < 0) {
